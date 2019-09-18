f111f3e5bb6d20faae583009bf213e7e67df583b
Melanie Legros <mlegro01@pc-gi-101.utbm.local>
2019-09-18 16:12:25 +0200

    Ajout du fileWrite

--- Server.cpp
+++ Server.cpp
@@ -21,9 +21,20 @@ void Server::consoleWrite(Server capteur)
 	cout << "Temperature : " << capteur.temperature << endl << "Humidite : " << capteur.humidite << endl << "Lumiere : " << capteur.lumiere << endl << "Sonore : " << capteur.sonore << endl;
 }
 
-void Server::fileWrite()
+void Server::fileWrite(Server capteur)
 {
-	
+	ofstream flux("donneeCapteur.txt");
+	if(flux)
+	{
+		flux << "Temperature : " << capteur.temperature << endl;
+		flux << "Humidite : " << capteur.humidite << endl;
+		flux << "Lumiere : " << capteur.lumiere << endl;
+		flux << "Sonore : " << capteur.sonore << endl;
+	}
+	else
+	{
+		cout << "Erreur : dans l'écriture d'un fichier" << endl;
+	}
 }
 
 Server::~Server()
