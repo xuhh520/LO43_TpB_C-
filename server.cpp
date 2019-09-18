b98c5cb182e7d23f4997a0fd1f3ce7124e9393fc
Mohamad Oueidat <moueid01@pc-gi-114.utbm.local>
2019-09-18 16:14:01 +0200

    sv

--- server.cpp
+++ server.cpp
@@ -1,1 +1,41 @@
+#include <iostream>
+using namespace std;
 
+
+class server
+{
+public:
+
+    float temp;
+    float hum;
+    float lum;
+    float son;
+
+
+
+public void consoleWrite(float temp,float hum,float lum, float son)
+{
+    cout << la temperature :" << temp << endl;
+    cout << la humidite :" << hum << endl;
+    cout << la lumiere :" << lum << endl;
+    cout << la temperature :" << temp << endl;
+    return 0;
+
+
+}
+
+
+
+
+
+public void fileWrite()
+{
+    printf("Salut, j'ai %d ans et je m'appelle %s\n", age, pseudo);
+}
+
+
+
+
+
+
+};
