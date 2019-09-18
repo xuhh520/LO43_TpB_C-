ddba49b7924425f5b9c033159384202a283bcc88
Honghao Xu <hoxu@pc-gi-108.utbm.local>
2019-09-18 19:03:58 +0200

    TP1

--- projet1.h
+++ projet1.h
@@ -1,12 +1,18 @@
-class Server
-{
-	public:
-		Server();
-		~Server();
-		Server(const Server&);
-		Server &operator=(const Server&);
-		Server consoleWrite ();
-		Server fileWrite ();
-
+class Server{
+	
+	int data_a,data_b;
+	
+public:
+	Server();
+	~Server();
+	Server(const Server&);
+	Server &operator=(const Server&);
+	Server &operator<<(const Server&);
+	//Server &operator<<(const Server&);
+	int consoleWrite ();
+	int fileWrite ();
+	int getData_a();
+	int getData_b();
+	
 
 }
