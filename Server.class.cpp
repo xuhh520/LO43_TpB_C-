98701b4565148551a4ccb7f509efb42f61d9cac6
Quentin Pugeat <qpugeat@pc-gi-115.utbm.local>
2019-09-18 17:58:17 +0200

    Ajout du fichier classe Server

---
+++ Server.class.cpp
@@ -0,0 +1,32 @@
+class Server
+{
+    public Server()
+    {
+
+    }
+
+    public Server(const Server& unAutreObjet)
+    {
+
+    }
+
+    virtual ~Server()
+    {
+
+    }
+
+    Server& operator=(const Server& s)
+    {
+
+    }
+
+    public void consoleWrite()
+    {
+
+    }
+
+    public void fileWrite()
+    {
+        
+    }
+}
