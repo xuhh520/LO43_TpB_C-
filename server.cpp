736b40fe70b33f9c8e1e9a84bdba6039253b6c02
Theau Zatti <tzatti@pc-gi-109.utbm.local>
2019-09-18 15:58:59 +0200

    ServerClass

--- server.cpp
+++ server.cpp
@@ -1,8 +1,44 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
 class Server{
 	
+	protected:
+		float temperature;
+		float humidite;
+		float lumiere;
+		float sonore;
+	
 	public:
-		Server();
-		Server(const Server&);
-		~Server();
-		Server &operator = (const Server&);
+		Server(); 		//Constructeur par défaut
+		
+		Server(float t,float h,float l,float s){
+			temperature = t;
+			humidite = h;
+			lumiere = l;
+			sonore = s;
+		};  //Constructeur
+		
+		Server(const Server&);	//Constructeur par recopie
+		~Server();	//Déstructeur
+		Server &operator = (const Server&);		//Opérateur d'affectation
+		
+	void consoleWrite(){
+		cout << "La température est :" << temperature <<"°C." << endl;
+		cout << "Le taux d'humidité est :" << humidite <<"%." << endl;
+		cout << "L'intensité lumineuse est de :" << lumiere <<"Lux." << endl;
+		cout << "Le niveau sonore est de :" << sonore <<"dB." << endl;
+	}
+	
+	void fileWrite(){
+		
+	}
+};
+
+int main(){
+	Server Serveur(35,25,24,25);
+	cout << "test" << endl;
+	Serveur.consoleWrite();
+	return 0;
 }
