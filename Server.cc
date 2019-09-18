666daf0d55cff247a92e5d1b7ac025de11c27986
朱睿清 <907671853@qq.com>
2019-09-18 21:06:25 +0200

    new

--- Server.cc
+++ Server.cc
@@ -3,7 +3,7 @@ std::ostream& operator<<(std::ostream& out, const Server& s){
     out<<s.a<<" "<<s.b;
     return out;
 }
-ofstream& operator<<(ofstream& out, const Server& s){
+std::ofstream& operator<<(std::ofstream& out, const Server& s){
     out<<s.a<<" "<<s.b;
     return out;}
 Server::Server(int first,int second){
