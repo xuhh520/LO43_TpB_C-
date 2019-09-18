cf3741b25cc9433243aea74799a837719d41f297
Quentin Pugeat <quentin.pugeat@hotmail.fr>
2019-09-18 18:15:42 +0200

    Ajout des fonctions consoleWrite et fileWrite

--- Server.class.cpp
+++ Server.class.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <fstream>
+
 class Server
 {
     public Server()
@@ -20,13 +23,16 @@ class Server
 
     }
 
-    public void consoleWrite()
+    public void consoleWrite(char strToDisplay)
     {
-
+        std::cout << strToDisplay;
     }
 
-    public void fileWrite()
+    public void fileWrite(char strToWrite)
     {
-        
+        ofstream logfile;
+        logfile.open("lo43Project.log");
+        logfile << strToWrite;
+        logfile.close();
     }
 }
